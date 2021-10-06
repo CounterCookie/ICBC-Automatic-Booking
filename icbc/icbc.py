82ca01408bc3d67a4ede601f1bb1c2842e4be811
CounterCookie <countercookio@gmail.com>
2021-10-06 05:41:44 -0700

    Finished icbc.py main script for now. Check for and list other available branches for a service Proper Try and except handling in while loops for ever. Look for blames with this hash, idk, already added the files Added HAR for booking a roadtest. as similliar as this one; but with authentication that I can hopefully do. Added the command file to run on desktop.

--- icbc/icbc.py
+++ icbc/icbc.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
-import json, requests, argparse, re
+import json, requests, argparse, re, time
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
 
 session = requests.Session()
+# Set max retries
+retries = Retry(total=5, backoff_factor=1, status_forcelist=[ 502, 503, 504, 403, 404 ])
+session.mount('https://', HTTPAdapter(max_retries=retries))
+
 parser = argparse.ArgumentParser()
 
 # Important regex
@@ -61,6 +68,12 @@ def findServices():
     servicesURL = 'https://onlinebusiness.icbc.com/qmaticwebbooking/rest/schedule/services'
     with session.get(servicesURL, headers=httpHeaders) as unparsedServices:
         services = json.loads(unparsedServices.text)
+
+        status = str(unparsedServices.status_code)
+        if status != "200":
+            print("HTTP error: " + status)
+            raise Exception("Error")
+
     for service in services:
         # Match the first letter of the object 'name'
         if service['name'][0] == serviceLetter:
@@ -82,6 +95,11 @@ def findBranches():
     with session.get(branchesURL, headers=httpHeaders) as unparsedCities:
         cities = json.loads(unparsedCities.text)
 
+        status = str(unparsedCities.status_code)
+        if status != "200":
+            print("HTTP error: " + status)
+            raise Exception("Error")
+
     for city in cities:
         # Replace spaces with nothing
         if city['addressZip'].replace(" ", "") == branchPostalCode.replace(" ", ""):
@@ -89,16 +107,24 @@ def findBranches():
             cityID = city['id']
             return cityID
     # The above return makes the function return a value and does not make it do anymore. in case of that failure, exit
-    print("Postal code not found or cityID for it could not be set.")
+    print("Postal code not found or cityID for the service could not be set; These cities provide it though:")
+    for availableCity in cities:
+        print(availableCity['addressCity'] + ' : ' + availableCity['addressLine1'] + ' : ' + availableCity['addressZip'])
     exit(1)
             
 
 
 def findDates():
-    datesURL = 'https://onlinebusiness.icbc.com/qmaticwebbooking/rest/schedule/branches/' + cityID + '/dates;servicePublicId=' + serviceID  + ';customSlotLength=10'
+    datesURL = 'https://onlinebusiness.icbc.com/qmaticwebbooking/rest/schedule/branches/' + cityID + '/dates;servicePublicId=' + serviceID
 
     with session.get(datesURL, headers=httpHeaders) as unparsedDates:
         dates = json.loads(unparsedDates.text)
+
+        status = str(unparsedDates.status_code)
+        if status != "200":
+            print("HTTP error: " + status)
+            raise Exception("Error")
+
     for d in range(int(minDay), int(maxDay)+1):     
         chosenDate = yearAndMonth + (str(d).zfill(2))
         for date in dates:
@@ -107,16 +133,20 @@ def findDates():
                 dateID = date['date']
                 print(dateID)
                 return dateID
-    print("Date not found.")
-    #TODO
-    exit(1)
 
+    raise Exception("Error")
 
 def findTimes():
-    timesURL = 'https://onlinebusiness.icbc.com/qmaticwebbooking/rest/schedule/branches/' + cityID + '/dates/' + dateID  + '/times;servicePublicId=' + serviceID  + ';customSlotLength=40'
+    timesURL = 'https://onlinebusiness.icbc.com/qmaticwebbooking/rest/schedule/branches/' + cityID + '/dates/' + dateID  + '/times;servicePublicId=' + serviceID
 
     with session.get(timesURL, headers=httpHeaders) as unparsedTimes:
         times = json.loads(unparsedTimes.text)
+
+        status = str(unparsedTimes.status_code)
+        if status != "200":
+            print("HTTP error: " + status)
+            raise Exception("Error")
+
     for h in range(int(minHour), int(maxHour)+1):
         for m in range(60):
             hm = str(h).zfill(2) + ":" + str(m).zfill(2)
@@ -127,18 +157,21 @@ def findTimes():
                     chosenHourAndMinutes = chosenTime['time']
                     print(chosenTime)
                     return chosenTime
-    print("Date not found.")
-    #TODO
-    exit(1)
 
+    raise Exception("Error")
 
 
 def reserve():
     formData = str('{"services":[{"publicId":"' + serviceID  + '"}],"custom":"{\\"peopleServices\\":[{\\"publicId\\":\\"' + serviceID  + '\\",\\"qpId\\":\\"' + serviceQPID  + '\\",\\"adult\\":1,\\"name\\":\\"' + serviceName  + '\\",\\"child\\":0}]}"}').encode('utf-8')
 
-    reserveURL = 'https://onlinebusiness.icbc.com/qmaticwebbooking/rest/schedule/branches/' + cityID + '/dates/' + dateID + '/times/' + chosenHourAndMinutes  + '/reserve;customSlotLength=40'
+    reserveURL = 'https://onlinebusiness.icbc.com/qmaticwebbooking/rest/schedule/branches/' + cityID + '/dates/' + dateID + '/times/' + chosenHourAndMinutes  + '/reserve'
 
     with session.post(reserveURL, headers=httpHeaders, data=formData) as reserveUnparsed:
+
+        status = str(reserveUnparsed.status_code)
+        if status != "200":
+            print("HTTP error: " + status)
+            raise Exception("Error")
         reserveParsed =  json.loads(reserveUnparsed.text)
         global reservePublicId
         reservePublicId = reserveParsed['publicId']
@@ -150,10 +183,16 @@ def checkMultiple():
 
     with session.get(checkMultipleURL, headers=httpHeaders) as checkMultiple:
         response = json.loads(checkMultiple.text)
+
+        status = str(checkMultiple.status_code)
+        if status != "200":
+            print("HTTP error: " + status)
+            raise Exception("Error")
+
         
     if response['message'] == 'ERROR_MULTI_GLOBAL':
         print("Customer already exists.")
-        exit(1)
+        raise Exception("Error")
 
 
 
@@ -164,18 +203,32 @@ def matchCustomer():
     matchCustomerURL = 'https://onlinebusiness.icbc.com/qmaticwebbooking/rest/schedule/matchCustomer'
 
     with session.post(matchCustomerURL, headers=httpHeaders, data=formData) as match:
+
+        status = str(match.status_code)
+        if status != "200":
+            print("HTTP error: " + status)
+            raise Exception("Error")
+
         response = json.loads(match.text)
     if response['allowOverwrite'] != False:
         print("Not allowed to overwrrite(?)")
-        exit(1)
-
+        raise Exception("Error")
 
 def bookAndConfirm():
     formData = str('{"customer":{"firstName":"' + firstName  + '","lastName":"' + lastName  + '","dateOfBirth":"' + dateOfBirth  + '","email":"' + emailAddress  + '","phone":"' + phoneNumber  + '","dob":"","externalId":""},"languageCode":"en","countryCode":"ca","notificationType":"","captcha":"","custom":"{\\"peopleServices\\":[{\\"publicId\\":\\"' + serviceID  + '\\",\\"qpId\\":\\"' + serviceQPID  + '\\",\\"adult\\":1,\\"name\\":\\"' + serviceName  + '\\",\\"child\\":0}],\\"totalCost\\":0,\\"createdByUser\\":\\"Qmatic Web Booking\\",\\"paymentRef\\":\\"\\",\\"customSlotLength\\":40}","notes":"","title":"Qmatic Web Booking"}').encode('utf-8')
 
     confirmUrl = 'https://onlinebusiness.icbc.com/qmaticwebbooking/rest/schedule/appointments/' + reservePublicId  + '/confirm'
-    with session.post(confirmUrl, headers=httpHeaders, data=formData) as confirm:
-        print(confirm.text)
+
+    # Spirits away
+    with session.post(confirmUrl, headers=httpHeaders, data=formData) as book:
+
+        status = str(book.status_code)
+        if status != "200":
+            print("HTTP error: " + status)
+            raise Exception("Error")
+            exit(1)
+
+    print("Booked")
     
 
 
@@ -183,6 +236,12 @@ def bookAndConfirm():
 
 # Set and get initial cookies and X-token required throughout form completion
 with session.get("https://onlinebusiness.icbc.com/qmaticwebbooking/rest/schedule/configuration") as initConfig:
+    print("\n\n\n\n\n")
+    status = str(initConfig.status_code)
+    if status != "200":
+        print("HTTP error: " + status)
+        raise Exception("Http 403 Error")
+
     openToken = json.loads(initConfig.text)['token']
 
     httpHeaders = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, l ike Gecko) Chrome/74.0.3729.169 Safari/537.36', 'Content-Type': 'application/json', 'X-CSRF-Token': openToken, 'Accept': 'application/json, text/plain, */*', 'Accept-Encoding': 'gzip, deflate, br', 'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8', 'Connection': 'keep-alive', 'Host': 'onlinebusiness.icbc.com', 'Origin': 'https://onlinebusiness.icbc.com', 'Referer': 'https://onlinebusiness.icbc.com/qmaticwebbooking/', 'Sec-Fetch-Site': 'same-origin'}
@@ -190,9 +249,23 @@ with session.get("https://onlinebusiness.icbc.com/qmaticwebbooking/rest/schedule
 
     findServices()
     findBranches()
-    findDates()
-    findTimes()
-    reserve()
-    checkMultiple()
-    matchCustomer()
-    #bookAndConfirm()
+    tryNum = 0
+    while True:
+        while True:
+            try:
+                matchCustomer()
+                findDates()
+                findTimes()
+                checkMultiple()
+            except:
+                tryNum += 1
+                print("Time or date not found; try count:  ", tryNum)
+                time.sleep(10)
+                break
+            reserve()
+            checkMultiple()
+            matchCustomer()
+            bookAndConfirm()
+            exit(0)
+            # TODO
+            # Add to for loop all availabe location for a service, when it fails.
